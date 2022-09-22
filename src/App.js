6849b5e9d44f65f7f9f1956586819403bbce8b45
Daniel Gomes <drg8703@gmail.com>
2022-09-22 15:23:11 -0300

    app multiplicador

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [num1, setnum1] = useState(0);
+  const [num2, setnum2] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
+  const calcular = () =>{
+    return parseFloat(num1) * parseFloat(num2);
+  }
+
+  useEffect(()=>{
+    setResultado(calcular())
+  }, [num1, num2]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Multiplicador de números</h1>
+          
+          <label>Primeiro número</label>
+            <input 
+              type="number" 
+              value = {num1}
+              onChange = {(e) => setnum1(e.target.value)}/>
+          
+          <label>Segundo número</label>
+            <input 
+              type="number"
+              value = {num2}
+              onChange = {(e) => setnum2(e.target.value)}/> 
+            
+            <label>Resultado: {resultado}</label>
+      </header> 
     </div>
   );
 }
